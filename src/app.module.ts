33d06a32c8bd75869ad81791f58693a8f8bb2d2b
arun1only1 <arun1nly1@gmail.com>
2025-01-22 23:49:42 +0545

    setup:db and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataFetchModule } from './data-fetch/data-fetch.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config/config.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DataFetchModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: configValidationSchema,
+      cache: true,
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    DataFetchModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.validation.ts
@@ -0,0 +1,18 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .required()
+    .valid('prod', 'dev', 'uat', 'stage')
+    .default('dev'),
+
+  API_PORT: Joi.number().default(8080),
+
+  SYSTEM_LANGUAGE: Joi.string().required().trim(),
+
+  DB_HOST: Joi.string().required().trim(),
+  DB_PORT: Joi.number().required(),
+  DB_USERNAME: Joi.string().required().trim(),
+  DB_PASSWORD: Joi.string().required().trim(),
+  DB_NAME: Joi.string().required().trim(),
+});
